b582defa55a54d5e1ebe0545f3002eab1fe92704
to3izo <to3izo3@gmail.com>
2023-12-20 10:52:52 +0900

    Button UI の画面実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+// Spartan UI
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HlmButtonDirective,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
